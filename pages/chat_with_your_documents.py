f46df123343b4f340246ddcfe2fc7187e8fe8c96
yupthatsme <48559374+yupthatsme@users.noreply.github.com>
2024-10-03 08:31:33 +0400

    Update chat_with_your_documents.py

--- pages/chat_with_your_documents.py
+++ pages/chat_with_your_documents.py
@@ -7,7 +7,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
-from pinecone import Pinecone
+import pinecone
 from dotenv import load_dotenv
 
 st.set_page_config(page_title="ChatDocs", page_icon="📄")
